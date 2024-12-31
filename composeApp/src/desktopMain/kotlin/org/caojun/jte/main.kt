a1e8c866f2e49a29701ae553dd49e86818ab592e
JadeCao <jadecao@tristarinc.com>
2024-12-31 09:46:08 +0800

    创建项目工程

---
+++ composeApp/src/desktopMain/kotlin/org/caojun/jte/main.kt
@@ -0,0 +1,13 @@
+package org.caojun.jte
+
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+
+fun main() = application {
+    Window(
+        onCloseRequest = ::exitApplication,
+        title = "JsonTariffEngine",
+    ) {
+        App()
+    }
+}
---
+++ composeApp/src/iosMain/kotlin/org/caojun/jte/MainViewController.kt
@@ -0,0 +1,5 @@
+package org.caojun.jte
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() = ComposeUIViewController { App() }
