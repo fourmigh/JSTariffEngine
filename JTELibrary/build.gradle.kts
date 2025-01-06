0720992ecba787a5b6fcbe1531f5012495f572c7
JadeCao <jadecao@tristarinc.com>
2025-01-06 13:22:50 +0800

    增加jte库

---
+++ JTELibrary/build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    kotlin("jvm") version "2.0.21"
+}
+
+group = "org.caojun.library.jte"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    implementation("com.google.code.gson:gson:2.10.1")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(17)
+}
